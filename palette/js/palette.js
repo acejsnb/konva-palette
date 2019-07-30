a53ddff75e74cd858c0b69b52d371c3f01a77902
xiongshuang <xsmails@163.com>
2019-07-30 10:17:09 +0800

    新增文字输入

--- palette/js/palette.js
+++ palette/js/palette.js
@@ -118,7 +118,104 @@ function drawRect(x, y, w, h, c, sw) {
 }
 
 /**
- * stage鼠标移动
+ * 输入文字
+ * @param x x坐标
+ * @param y y坐标
+ * @param fill 文字颜色
+ * @param fs 文字大小
+ */
+function drawText(x, y, fill, fs) {
+    var text = new Konva.Text({
+        text: '双击编辑文字',
+        x: x,
+        y: y,
+        fill: fill,
+        fontSize: fs,
+        width: 300,
+        draggable: true
+    });
+    graphNow=text;
+    layer.add(text);
+    layer.draw();
+
+    text.on('mouseenter', function() {
+        stage.container().style.cursor = 'move';
+    });
+
+    text.on('mouseleave', function() {
+        stage.container().style.cursor = 'default';
+    });
+
+    text.on('dblclick', function() {
+        // 在画布上创建具有绝对位置的textarea
+
+        // 首先，我们需要为textarea找到位置
+
+        // 首先，让我们找到文本节点相对于舞台的位置:
+        let textPosition = this.getAbsolutePosition();
+
+        // 然后让我们在页面上找到stage容器的位置
+        let stageBox = stage.container().getBoundingClientRect();
+
+        // 因此textarea的位置将是上面位置的和
+        let areaPosition = {
+            x: stageBox.left + textPosition.x,
+            y: stageBox.top + textPosition.y
+        };
+
+        // 创建textarea并设置它的样式
+        let textarea = document.createElement('textarea');
+        document.body.appendChild(textarea);
+
+        let T=this.text();
+        if (T === '双击编辑文字') {
+            textarea.value = '';
+            textarea.setAttribute('placeholder','请输入文字')
+        } else {
+            textarea.value = T;
+        }
+
+        textarea.style.position = 'absolute';
+        textarea.style.top = areaPosition.y + 'px';
+        textarea.style.left = areaPosition.x + 'px';
+        textarea.style.background = 'none';
+        textarea.style.border = '1px dashed #000';
+        textarea.style.outline = 'none';
+        textarea.style.color = this.fill();
+        textarea.style.width = this.width();
+
+        textarea.focus();
+
+        this.setAttr('text', '');
+        layer.draw();
+
+        // 确定输入的文字
+        let confirm=(val) => {
+            this.text(val?val:'双击编辑文字');
+            layer.draw();
+            // 隐藏在输入
+            if (textarea) document.body.removeChild(textarea);
+        };
+        // 回车键
+        let keydown=(e) => {
+            if (e.keyCode === 13) {
+                textarea.removeEventListener('blur', blur);
+                confirm(textarea.value)
+            }
+        };
+        // 鼠标失去焦点
+        let blur=() => {
+            textarea.removeEventListener('keydown', keydown);
+            confirm(textarea.value);
+        };
+
+        textarea.addEventListener('keydown', keydown);
+        textarea.addEventListener('blur', blur);
+    });
+}
+
+/**
+ * stage鼠标按下
  * @param flag 是否可绘制
  * @param ev 传入的event对象
  */
@@ -140,6 +237,9 @@ function stageMousedown(flag, ev) {
                 break;
             case 'rectH':
                 drawRect(x, y, 0, 0, 'red', 2);
+                break;
+            case 'text':
+                drawText(x, y, 'red', 16);
                 break;
             default:
                 break;
